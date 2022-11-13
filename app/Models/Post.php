2133fee8e0f9e229d47f5b786e44a5db59c11331
Navid Bakhtiary <NavidBakhtiary@gmail.com>
2022-11-13 15:28:57 +0330

    Fix: Change user_id to admin_id in post entity

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,7 +10,7 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = [
-        'user_id', 'subject', 'content'
+        'admin_id', 'subject', 'content'
     ];
 
     public function comments()
