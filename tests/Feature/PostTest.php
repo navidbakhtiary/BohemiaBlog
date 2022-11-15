b616bbc946428716f65c91ca2e786a60aae99bd8
Navid Bakhtiary <NavidBakhtiary@gmail.com>
2022-11-15 11:01:20 +0330

    Test: Create test function to ensure get failed message of saving with invalid inputs

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Classes\HttpStatus;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Faker\Factory;
 
 class PostTest extends TestCase
 {
@@ -52,5 +54,39 @@ class PostTest extends TestCase
                 ]
             );
         $this->assertDatabaseHas('posts', array_merge(['admin_id' => $admin->id], $attributes));
+    }
+
+    public function testNewPostSavingWithInvalidInputDataIsUnsuccessful()
+    {
+        $user = User::factory()->create();
+        $admin = $user->admin()->create();
+        $token = $user->createToken('test-token');
+        $subject = Factory::create()->paragraph();
+        $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . $token->plainTextToken])->
+            postJson($this->api_save, ['subject' => $subject]);
+        $response->assertStatus(HttpStatus::BadRequest)->assertJson(
+                [
+                    'message' => [
+                        'code' => 'E091',
+                        'text' => 'Inputs are invalid.'
+                    ],
+                    'errors' =>
+                    [
+                        'subject' => [
+                            [
+                                'code' => '1902-57',
+                                'message' => 'The subject must not have more than 64 characters.'
+                            ]
+                        ],
+                        'content' => [
+                            [
+                                'code' => '0302-72',
+                                'message' => 'The content field is required.'
+                            ]
+                        ],
+                    ]
+                ]
+            );
+        $this->assertDatabaseMissing('posts', ['admin_id' => $admin->id, 'subject' => $subject]);
     }
 }
