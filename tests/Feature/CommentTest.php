5bc41dec4379ade6fa50109486a81cb4c1ed3de9
Navid Bakhtiary <NavidBakhtiary@gmail.com>
2022-11-16 16:36:37 +0330

    Test: New test function has been added to check new comment can not be save with invalida inputs

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -3,9 +3,11 @@
 namespace Tests\Feature;
 
 use App\Classes\Creator;
+use App\Classes\HttpStatus;
 use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
+use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -38,4 +40,43 @@ class CommentTest extends TestCase
         $this->assertDatabaseHas('comments', 
             ['user_id' => $user->id, 'post_id' => $post->id, 'content' => $comment->content]);
     }
+
+    public function testNewCommentSavingWithInvalidInputDataIsUnsuccessful()
+    {
+        $user = User::factory()->create();
+        $admin = $user->admin()->create();
+        $post = Post::factory()->create();
+        $token = $user->createToken('test-token');
+        $content = Factory::create()->paragraph(40);
+        $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . $token->plainTextToken])->
+            postJson(str_replace('{id}', $post->id, $this->api_save), ['content' => Factory::create()->sentences()]);
+        $response->assertStatus(HttpStatus::BadRequest)->assertJson(
+            [
+                'message' => Creator::createFailureMessage('invalid_inputs'),
+                'errors' =>
+                [
+                    'content' => [
+                        Creator::createValidationError('content', 'string', null, true)
+                    ],
+                ]
+            ]
+        );
+        $this->assertDatabaseMissing('comments', ['user_id' => $user->id, 'post_id' => $post->id]);
+
+        $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . $token->plainTextToken])->
+            postJson(str_replace('{id}', $post->id, $this->api_save), ['content' => $content]);
+        $response->assertStatus(HttpStatus::BadRequest)->assertJson(
+            [
+                'message' => Creator::createFailureMessage('invalid_inputs'),
+                'errors' =>
+                [
+                    'content' => [
+                        Creator::createValidationError('content', 'max.string', null, true, ['max' => '500'])
+                    ],
+                ]
+            ]
+        );
+        $this->assertDatabaseMissing('comments', ['user_id' => $user->id, 'post_id' => $post->id, 'content' => $content]);
+    }
+
 }
