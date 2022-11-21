024a0697212617346d2d0cff862b20bc3340b11f
Navid Bakhtiary <NavidBakhtiary@gmail.com>
2022-11-21 19:19:45 +0330

    Test: Create test function to check that admin can get list of deleted posts

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -20,6 +20,7 @@ class PostTest extends TestCase
     private $api_delete = '/api/post/{post_id}/delete';
     private $api_list = '/api/post/list';
     private $api_show = '/api/post/{post_id}';
+    private $api_trash_list = '/api/trash/post/list';
 
     public function testCreatePostByAdmin()
     {
@@ -311,5 +312,64 @@ class PostTest extends TestCase
         $post = Post::factory()->create();
         $response = $this->getJson(str_replace('{post_id}', 2, $this->api_show));
         $response->assertNotFound()->assertJson(['message' => Creator::createFailureMessage('post_not_found'), 'errors' => []]);
+    }
+
+    public function testAdminCanGetListOfDeletedPosts()
+    {
+        $factory = Factory::create();
+        $user = User::factory()->create();
+        $admin = $user->admin()->create();
+        $token = $user->createToken('test-token');
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+        $post1 = Post::factory()->create();
+        $post2 = Post::factory()->create();
+        $comment1 = $post1->comments()->create(['user_id' => $user1->id, 'content' => $factory->paragraph()]);
+        $comment2 = $post2->comments()->create(['user_id' => $user1->id, 'content' => $factory->paragraph()]);
+        $comment3 = $post1->comments()->create(['user_id' => $user2->id, 'content' => $factory->paragraph()]);
+        $comment4 = $post2->comments()->create(['user_id' => $user2->id, 'content' => $factory->paragraph()]);
+        $comment5 = $post1->comments()->create(['user_id' => $user2->id, 'content' => $factory->paragraph()]);
+        $post1->delete();
+        $post2->delete();
+        $response = $this->withHeaders(['Authorization' => $this->bearer_prefix . $token->plainTextToken])->
+            getJson($this->api_trash_list);
+        $response->assertOk()->
+            assertJsonFragment(['message' => Creator::createSuccessMessage('deleted_posts_list')])->
+            assertJsonStructure(['message', 'data' => ['deleted posts' => []], 'pagination' => []])->
+            assertJsonFragment([
+                'deleted posts' =>
+                [
+                    [
+                        'id' => $post1->id,
+                        'subject' => $post1->subject,
+                        'created at' => $post1->created_at,
+                        'deleted at' => $post1->deleted_at,
+                        'summary' => substr($post1->content, 0, 250),
+                        'comments count' => 3,
+                        'author' => [
+                            'id' => $user->id,
+                            'name' => $user->name,
+                            'surname' => $user->surname,
+                            'nickname' => $user->nickname,
+                            'username' => $user->username,
+                        ]
+                    ],
+                    [
+                        'id' => $post2->id,
+                        'subject' => $post2->subject,
+                        'created at' => $post1->created_at,
+                        'deleted at' => $post1->deleted_at,
+                        'summary' => substr($post2->content, 0, 250),
+                        'comments count' => 2,
+                        'author' => [
+                            'id' => $user->id,
+                            'name' => $user->name,
+                            'surname' => $user->surname,
+                            'nickname' => $user->nickname,
+                            'username' => $user->username,
+                        ]
+                    ]
+                ]
+            ]);
     }
 }
